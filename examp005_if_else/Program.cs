ba550b757b3d04e153dd6eeabb061fa6c85d32fd
Andrew <tropterop@yandex.ru>
2022-09-28 20:03:32 +0300

    examp006

---
+++ examp005_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Add username:");
+string username = Console.ReadLine();
+
+if(username == "Masha")
+{
+    Console.WriteLine("Wow, this is Masha");
+}
+else{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
